f77b6d538dea1db6d6db8f2ec7cc382dc41c81f6
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2024-08-20 02:50:31 +0530

    create the header and profile page private

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { signin, signup } from '../controllers/auth.controller.js';
 import { google, signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
-import Header from "./components/HEader";
 
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import PrivateRoute from './components/PrivateRoute';
 export default function App() {
   //return <h1 className='text-red-500'>App</h1>
   return (
@@ -18,7 +19,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute/>}>
         <Route path="/profile" element={<Profile />} />
+      </Route>
       </Routes>
     </BrowserRouter>
   );
